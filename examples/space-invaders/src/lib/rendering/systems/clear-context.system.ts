d16fe572278f362972ef8b6185a7a605cd42be15
Ana73l <b0b094abv@gmail.com>
2022-11-17 03:05:42 +0200

    Refactoring space-invaders to complete relevant jobs before rendering

--- examples/space-invaders/src/lib/rendering/systems/clear-context.system.ts
+++ examples/space-invaders/src/lib/rendering/systems/clear-context.system.ts
@@ -1,8 +1,7 @@
-import { group, read, System } from '@dark-star/ecs';
+import { group, System } from '@dark-star/ecs';
 import { injectable } from '@dark-star/di';
+
 import { RenderGroupSystem } from './render-group.system';
-import { Sprite } from '../components/sprite.data';
-import { Position } from '../../movement/components/position.data';
 
 @injectable()
 @group(RenderGroupSystem)
@@ -12,7 +11,6 @@ export class ClearContextSystem extends System {
 	}
 
 	public override async update(): Promise<void> {
-		await this.completeJobs([read(Sprite), read(Position)]);
 		this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
 	}
 }
--- examples/space-invaders/src/lib/rendering/systems/render-group.system.ts
+++ examples/space-invaders/src/lib/rendering/systems/render-group.system.ts
@@ -1,5 +1,13 @@
-import { SystemGroup } from '@dark-star/ecs';
+import { read, SystemGroup } from '@dark-star/ecs';
 import { injectable } from '@dark-star/di';
 
+import { Position } from '../../movement/components/position.data';
+import { Sprite } from '../components/sprite.data';
+
 @injectable()
-export class RenderGroupSystem extends SystemGroup {}
+export class RenderGroupSystem extends SystemGroup {
+	public override async update(): Promise<void> {
+		// complete all jobs using rendering data before rendering phase begins
+		await this.completeJobs([read(Sprite), read(Position)]);
+	}
+}
